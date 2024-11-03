b7484f97b161aee5732f3fa1e61afb4499dbdb89
RouSage <rousage@protonmail.com>
2024-11-03 14:23:41 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing Oh My Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash

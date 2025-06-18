04b9bf9eeea618851a332a3532288985faa4b715
RouSage <rousage@protonmail.com>
2025-06-18 21:42:15 +0200

    SQL Formatter for Zed

--- install.sh
+++ install.sh
@@ -9,8 +9,12 @@ brew install chezmoi
 echo "Installing Oh My Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Installing NVM"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
 echo "Installing gopls"
 go install golang.org/x/tools/gopls@latest
+
+echo "Installing NVM"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+echo "Installing LTS Node.js version"
+nvm install --lts
+echo "Installing sql-formatter"
+npm install -g sql-formatter
